5f55535bba9f565c406cdd48836f4444cfd87c7c
sangmin1120 <85210970+sangmin1120@users.noreply.github.com>
2024-02-14 21:38:33 +0900

    DP : 시간 초과(고민) , 구간 합

---
+++ DP/11660.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define MAX 1206
+using namespace std;
+int x1,x2,y1,y2;
+int n;
+int cnt;
+int dp[MAX][MAX];
+int temp;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> cnt;
+
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){
+            cin >> temp;
+            dp[i][j] = dp[i-1][j]+dp[i][j-1]+temp-dp[i-1][j-1];
+        }
+    }
+
+    for (int i=0;i<cnt;i++){
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1] << '\n';
+    }
+    return 0;
+}
