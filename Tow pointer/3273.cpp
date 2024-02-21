98aaf8f4b431b1aa1ea9be2fc02edb09407253f7
sangmin1120 <sangmin8276@gmail.com>
2024-02-21 13:30:56 +0900

    Two pointer : 두 수의 합

---
+++ Tow pointer/3273.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 100001
+using namespace std;
+
+int n;
+
+int target;
+int res = 0;
+
+void solution(){
+    sort(v)
+}
+int main(){
+    // input & init
+    cin >> n;
+    v.
+    for (int i=0;i<n;i++)   cin >> v[i];
+    cin >> target;
+
+    solution();
+
+    return 0;
+}
