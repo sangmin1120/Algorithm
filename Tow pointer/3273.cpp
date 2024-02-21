6f0eec9f430642dc1efb5e4a171186a1b425b7e1
sangmin1120 <sangmin8276@gmail.com>
2024-02-21 13:39:07 +0900

    Two pointer : 두 수의 합

--- Tow pointer/3273.cpp
+++ Tow pointer/3273.cpp
@@ -5,18 +5,34 @@
 using namespace std;
 
 int n;
-
+int arr[MAX];
 int target;
 int res = 0;
 
 void solution(){
-    sort(v)
+    // 정렬
+    sort(arr,arr+n);
+
+    int s = 0;
+    int e = n-1;
+    // target을 기준으로 s , e를 조절한다. (Two pointer)
+    while (s<e){
+        int sum = arr[s] + arr[e];
+        if (sum < target)
+            s++;
+        else if (sum > target)
+            e--;
+        else if (sum == target){
+            res++;
+            e--;
+        }
+    }
+    cout << res << endl;
 }
 int main(){
     // input & init
     cin >> n;
-    v.
-    for (int i=0;i<n;i++)   cin >> v[i];
+    for (int i=0;i<n;i++)   cin >> arr[i];
     cin >> target;
 
     solution();
