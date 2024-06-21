736154aef88f74aff86c65d01cbe1d7a27efc5b2
sangmin1120 <sangmin8276@gmail.com>
2024-06-21 19:39:53 +0900

    Greddy : 단어 수학

---
+++ Greedy/1339.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+string input;
+int letter_count[10] = {0};
+
+bool cmp(int a,int b){
+    return a>b;
+}
+void solution(){
+    for (int i=0;i<n;i++){
+        string s;
+        cin >> s;
+        int val = 1;
+        for (int j=s.size()-1;j>=0;j--){
+            letter_count[s[j]-'A'] += val;
+            val *= 10;
+        }
+    }
+
+    sort(letter_count,letter_count+10,cmp);
+
+    int sum = 0;
+    int num = 9;
+    for (int i=0;i<10;i++){
+        sum += letter_count[i]*num;
+        num--;
+    }
+
+    cout << sum << endl;
+
+}
+int main(){
+    cin >> n;
+    
+    solution();
+
+    return 0;
+}
