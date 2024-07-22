a29e8d9155cda48f13dba215a62f3e76f6e1226f
sangmin1120 <sangmin8276@gmail.com>
2024-07-22 15:04:11 +0900

    Graph: 다익스트라
    코드 정리

--- BOJ/Graph/1753.cpp
+++ BOJ/Graph/1753.cpp
@@ -1,55 +1,66 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#define INF 987654321
-#define MAX 20001
 using namespace std;
 
-int n,e;
-vector <pair<int,int>> arr[MAX];
-int dist[MAX];
+#define MAX 20001
+#define INF 987654321
+// directed weight graph
+int v,e;
 int start;
+vector<pair<int,int>> graph[MAX]; // graph[a] = {w,v};
+int dist[MAX];
 
-void solve(){
-    priority_queue<pair <int,int>> pq;
-    pq.push(make_pair(0,start)); // weight, start_vertex
-    dist[start] = 0;
-
-    while (!pq.empty()){
-        int vertex = pq.top().second;
-        int cost = -pq.top().first;
-        pq.pop();
-
-        for (int i=0;i<arr[vertex].size();i++){
-            int next = arr[vertex][i].first;
-            int next_cost = arr[vertex][i].second;
-
-            if (dist[next] > cost+next_cost){ // alarm clock
-                dist[next] = cost+next_cost;
-                pq.push(make_pair(-dist[next],next));
-            }
-        }
-    }
-}
-int main(){
-    cin >> n >> e; // vertex 수와 edge 수
+void init(){
+    cin >> v >> e;
     cin >> start;
 
     for (int i=0;i<e;i++){
-        int a,b,c;
-        cin >> a >> b >> c;
-        arr[a].push_back(make_pair(b,c));
-    } // 입력 받기
-
-    for (int i=1;i<=n;i++)
-        dist[i] = INF; // 무한 대로 초기화
-    solve();
-
-    for (int i=1;i<=n;i++){
-        if (dist[i] == INF)
-            cout << "INF" << '\n';
-        else
-            cout << dist[i] << '\n';
+        int a,b,w;
+        cin >> a >> b >> w;
+        graph[a].push_back(make_pair(w,b));
     }
+
+    fill(dist,dist+v+1,INF);
+}
+void solution(){
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    dist[start]=0;
+    pq.push(make_pair(0,start));
+
+    while(!pq.empty()){
+        auto cur = pq.top();
+        pq.pop();
+
+        int d=cur.first;
+        int idx=cur.second;
+
+        if (dist[idx] != d) continue;
+
+        for (auto next : graph[idx]){
+            int next_d = next.first;
+            int next_idx = next.second;
+
+            if (dist[next_idx]>dist[idx]+next_d){
+                dist[next_idx] = dist[idx]+next_d;
+                pq.push(make_pair(dist[next_idx],next_idx));
+            }
+        }
+    }
+
+    for (int i=1;i<=v;i++){
+        if (dist[i]==INF)   cout << "INF\n";
+        else    cout << dist[i] << '\n';
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    init();
+
+    solution();
+
     return 0;
 }
