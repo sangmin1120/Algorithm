3ec532d8f99673c09fd09fb021ccd72af8c11b40
sangmin1120 <sangmin8276@gmail.com>
2024-07-23 12:44:37 +0900

    Graph: floyd 알고리즘

--- BOJ/Graph/11404.cpp
+++ BOJ/Graph/11404.cpp
@@ -1,56 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define MAX 100001
-#define INF 987654321
 using namespace std;
 
-int n,m;
-vector<pair<int,int>> v[MAX];
-vector<int> dist[MAX];
+#define MAX 101
+#define INF 987654321
 
-void solve(){
+int n;
+int m;
+int dist[MAX][MAX];
+
+void init(){
+    cin >> n;
+    cin >> m;
+
+    for (int i=1;i<=n;i++){
+        fill(dist[i],dist[i]+1+n,INF);
+    }
+
+    for (int i=0;i<m;i++){
+        int a,b,w;
+        cin >> a >> b >> w;
+        dist[a][b] = min(dist[a][b],w);
+    }
+
+    for (int i=1;i<=n;i++)
+        dist[i][i]=0;
+
+}
+void solution(){
+
     for (int k=1;k<=n;k++){
         for (int i=1;i<=n;i++){
-            for (int j=1;j<=n;j++){
+            for (int j=1;j<=n;j++)
                 dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
-            }
         }
     }
 
     for (int i=1;i<=n;i++){
         for (int j=1;j<=n;j++){
-            if (dist[i][j] == INF)
-                cout << '0' << ' ';
-            else
-                cout << dist[i][j] << ' ';
+            if (dist[i][j]==INF)    cout << "0 ";
+            else    cout << dist[i][j]  << ' ';
         }
         cout << '\n';
     }
 }
 int main(){
-    cin >> n;
-    cin >> m;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    // dist 초기화
-    for (int i=1;i<=n;i++){
-        dist[i].resize(n);
-        for (int j=1;j<=n;j++){
-            if (i==j)
-                dist[i][j] = 0;
-            else
-                dist[i][j] = INF;
-        }
-    }
+    init();
+    
+    solution();
 
-    // edge 입력
-    for (int i=1;i<=m;i++){
-        int a,b,c;
-        cin >> a >> b >> c;
-        v[a].push_back(make_pair(b,c));       // a에서 b가는 c비용
-        dist[a][b] = min(dist[a][b],c);  // 가는 길이 여러 개 일 수 있음.
-    }
-
-    solve();
     return 0;
 }
