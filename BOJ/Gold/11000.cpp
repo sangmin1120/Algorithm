a228515544809b04d3e709703b2c81c72ffddccc
sangmin1120 <sangmin8276@gmail.com>
2025-05-15 20:02:04 +0900

    [greedy] - G5 : 강의실 배정

---
+++ BOJ/Gold/11000.cpp
@@ -0,0 +1,44 @@
+/**
+ * 강의실 배정: S에서 시작해 t에 끝나는 N개 수업 -> 최소의 강의실을 사용
+ * greedy
+ */
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<pair<int,int>> t;
+void solution(){
+    sort(t.begin(),t.end());
+
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    pq.push(t[0].second);
+    int count = 1;
+
+    for (int i=1;i<t.size();i++){
+        if (t[i].first < pq.top()){
+            pq.push(t[i].second);
+        }
+        else{
+            pq.pop();
+            pq.push(t[i].second);
+        }
+    }
+    cout << pq.size() << '\n';
+}   
+int main(){
+    // input & init
+    cin >> n;
+    for (int i=0;i<n;i++){
+        int a,b;
+        cin >> a >> b;
+        t.push_back(make_pair(a,b));
+    }
+
+    solution();
+
+    return 0;
+}
