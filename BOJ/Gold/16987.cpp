47c14519675e3932f09e4e652e91ce8428ec6b90
sangmin1120 <sangmin8276@gmail.com>
2025-08-15 19:48:29 +0900

    '~ing'

---
+++ BOJ/Gold/16987.cpp
@@ -0,0 +1,18 @@
+/**
+ * 계란으로 계란치기
+ * BACKTRAKING
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    solution();
+
+    return 0;
+}
