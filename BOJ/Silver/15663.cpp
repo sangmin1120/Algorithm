9045ac0408267a35f565e730dbffd7dce8824adf
sangmin1120 <sangmin8276@gmail.com>
2025-08-22 19:43:03 +0900

    [BT] - S3 : N과 M(9)

--- BOJ/Silver/15663.cpp
+++ BOJ/Silver/15663.cpp
@@ -8,7 +8,29 @@ using namespace std;
 
 int n,m;
 vector<int> v;
+int visited[10];
+vector<int> res;
 
+void bt(int count, int prev) {
+    // exit cond
+    if (count==m) {
+        for (auto r : res)
+            cout << r << ' ';
+        cout << '\n';
+    }
+
+    //logic
+    int temp=0;
+    for (int i=0;i<n;i++) {
+        if (!visited[i] && prev!=i) {
+            visited[i] = 1;
+            res.push_back(v[i]);
+            bt(count+1,i);
+            visited[i] = 0;
+            res.pop_back();
+        }
+    }
+}
 void solution() {
 
     sort(v.begin(),v.end());
