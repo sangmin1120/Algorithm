2f284f16a5e8011c11631c5db9993680b9b71380
sangmin1120 <sangmin8276@gmail.com>
2025-08-23 16:08:10 +0900

    [BT] - S2 : N과 M (9)

--- BOJ/Silver/15663.cpp
+++ BOJ/Silver/15663.cpp
@@ -1,6 +1,5 @@
 /**
  * N과 M(9) - n 개의 자연수 중에서 m 개 선택, 중복되는 수열 없이 출력
- * before, prev 이용
  * BACKTRACKING (S2)
  */
 #include <bits/stdc++.h>
@@ -11,21 +10,23 @@ vector<int> v;
 int visited[10];
 vector<int> res;
 
-void bt(int count, int prev) {
+void bt(int count) {
     // exit cond
     if (count==m) {
         for (auto r : res)
             cout << r << ' ';
         cout << '\n';
+        return;
     }
 
     //logic
     int temp=0;
     for (int i=0;i<n;i++) {
-        if (!visited[i] && prev!=i) {
+        if (!visited[i] && temp!=v[i]) {
             visited[i] = 1;
             res.push_back(v[i]);
-            bt(count+1,i);
+            temp = v[i];
+            bt(count+1);
             visited[i] = 0;
             res.pop_back();
         }
@@ -35,7 +36,7 @@ void solution() {
 
     sort(v.begin(),v.end());
 
-    
+    bt(0);
 }
 int main() {
     ios::sync_with_stdio(0);
