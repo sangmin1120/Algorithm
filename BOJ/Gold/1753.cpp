7ab556bda3fceb6af66c830743959fda27fc3ccb
sangmin1120 <sangmin8276@gmail.com>
2025-09-29 19:52:41 +0900

    다익스트라 ~ing

--- BOJ/Gold/1753.cpp
+++ BOJ/Gold/1753.cpp
@@ -9,11 +9,38 @@ using namespace std;
 // init
 int v,e; // vertex의 수, edge의 수
 int start;
-int graph[INF_MAX][INF_MAX]; // v -> u 의 w 가중치
-int result
+vector<pair<int,int>> graph[20000]; // v -> u 의 w 가중치
+vector<int> visited(20001,INT_MAX);
 
 void solution() {
 
+    priority_queue<pair<int,int>> pq; // weight, v
+    visited[start] = 0;
+    pq.push({0,start});
+
+    while (!pq.empty()) {
+        int cur_dist = - pq.top().first;
+        int cur_node = pq.top().second;
+        pq.pop();
+
+        for (int i=0;i<graph[cur_node].size();i++) {
+            int next_node = graph[cur_node][i].first;
+            int next_dist = cur_dist + graph[cur_node][i].second;
+
+            if (next_dist < visited[next_node]){
+                visited[next_node] = next_dist;
+                pq.push({-next_dist,next_node});
+            }
+        }
+    }
+
+    // printf
+    for (int i=1;i<=v;i++) {
+        if (visited[i]==INT_MAX)
+            cout << "INF" << '\n';
+        else
+            cout << visited[i] << '\n';
+    }
 }
 int main() {
     ios::sync_with_stdio(0);
@@ -24,7 +51,7 @@ int main() {
     cin >> start;
     for (int i=0;i<e;i++) {
         int a,b,w;  cin >> a >> b >> w;
-        graph[a][b] = w;
+        graph[a].push_back({b,w});
     }
 
     solution();
